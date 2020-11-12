50afe5fda3a641331ba640045e59e03f3e4168e5
Andreas Hofheinz <andreas.hofheinz@outlook.com>
2020-11-12 10:58:48 +0100

    Adds argument `output_type` to kaya function incl. documentation.
    Refers to #1

--- R/kaya.R
+++ R/kaya.R
@@ -9,15 +9,24 @@
 #' @param gdp GDP per capita (in 1000$/person).
 #' @param enInt Energy Intensity (in Gigajoule/$1000GDP).
 #' @param carbInt Carbon Intensity (in tonnes CO2/Gigajoule).
-#' @return Total yearly CO2 emissions according to the kaya identity.
+#' @param output_type Output type can be either "CO2" or "C".
+#' @return Total yearly emissions according to the kaya identity in CO2 or C.
 #' @examples
 #' # Kaya Identity for Germany
 #' kaya(82.4, 44, 5, 0.05)
+#' # Kaya Identity for Germany in C (Carbon)
+#' kaya(82.4, 44, 5, 0.05, output_type = "C")
 #' @export
-kaya <- function(pop, gdp, enInt, carbInt) {
+kaya <- function(pop, gdp, enInt, carbInt, output_type = "CO2") {
   checkmate::assert_numeric(pop, lower = 0, len = 1, any.missing = FALSE)
   checkmate::assert_numeric(gdp, lower = 0, len = 1, any.missing = FALSE)
   checkmate::assert_numeric(enInt, lower = 0, len = 1, any.missing = FALSE)
   checkmate::assert_numeric(carbInt, lower = 0, len = 1, any.missing = FALSE)
-  pop * gdp * enInt * carbInt
+  checkmate::assert_character(output_type, any.missing = FALSE)
+  stopifnot(output_type %in% c("CO2", "C"))
+  emissions <- pop * gdp * enInt * carbInt
+  if (output_type == "C") {
+    emissions <- emissions / 3.67
+  }
+  emissions
 }
